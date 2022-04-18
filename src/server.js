68b579a51a8550d73b68ba6697804ac1b7e5a0f5
Bruno Jullien <bruno.jullien@gmail.com>
2022-04-18 11:01:37 -0700

    Adding MongoDB to Express

--- src/server.js
+++ src/server.js
@@ -1,29 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-const articlesInfo = {
-  'learn-react': {
-    upvotes: 0,
-    comments: [],
-  },
-  'learn-node': {
-    upvotes: 0,
-    comments: [],
-  },
-  'my-thoughts-on-resumes': {
-    upvotes: 0,
-    comments: [],
-  },
-}
+import { MongoClient } from "mongodb";
 
 const app = express();
 
 //POST requests will be parsed, req.body added and JSON extracted
 app.use(bodyParser.json());
 
+app.get('/api/articles/:name', async (req, res) => {
+  const articleName = req.params.name;
+
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('my-blog');
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (err) {
+    res.status(500).json({ message: 'Error connecting to db', err });
+  }
+});
+
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
-  articlesInfo[articleName].upvotes++;
+
+
   res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`);
 })
 
